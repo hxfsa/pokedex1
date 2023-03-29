ca15b8efeac5a490029bedca57caa70ba6bc1af6
Hafsa <hafsa.aitouarraou@outlook.fr>
2023-03-29 11:41:46 +0200

    premier exo props

--- pokedex1/src/App.jsx
+++ pokedex1/src/App.jsx
@@ -3,9 +3,23 @@ import PokemonCard from "./components/PokemonCard";
 function App() {
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard name = {pokemonList[0].name} imgSrc = {pokemonList[0].imgSrc} />
     </div>
   );
 }
+
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+
 
 export default App;
--- pokedex1/src/components/PokemonCard.jsx
+++ pokedex1/src/components/PokemonCard.jsx
@@ -1,6 +1,5 @@
-function PokemonCard() {
-  const pokemon = pokemonList[0];
-  const {name, imgSrc} = pokemon;
+function PokemonCard({name, imgSrc}) {
+  
 
   return (
     <figure>
@@ -10,16 +9,7 @@ function PokemonCard() {
   );
 }
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+
 
 
 export default PokemonCard;
