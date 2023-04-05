a42d42f634c90719b29c826ed61076e424e456bd
Hafsa <hafsa.aitouarraou@outlook.fr>
2023-04-05 10:49:03 +0200

    use effect alerts

--- pokedex1/src/App.jsx
+++ pokedex1/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useEffect } from "react";
 
 import PokemonCard from "./components/PokemonCard";
 import NavBar from "./components/NavBar";
@@ -30,6 +31,12 @@ const pokemonList = [
   ];
 
 function App() {
+  useEffect(
+    () => {
+      alert("hello pokemon trainer :)");
+    },
+    []
+  );
   const [pokemonIndex, setPokemonIndex] = useState(0);
   const decrementPokemon = () => {
     if (pokemonIndex > 0) {
--- pokedex1/src/components/NavBar.jsx
+++ pokedex1/src/components/NavBar.jsx
@@ -2,10 +2,14 @@ import App from "../App";
 
 function NavBar(props) {
    const { decrementPokemon, incrementPokemon, pokemonIndex, pokemonList } = props;
+   if(pokemonIndex === [3]) {
+    alert("pika pikachu !!!")
+   }
     return (
-        <div style={{display:'flex'}}>
+        <div >
    {pokemonIndex > 0 ?  <button onClick={decrementPokemon}>Précédent</button> : "" }
       {pokemonIndex < pokemonList.length -1 ? <button onClick={incrementPokemon}>Suivant</button> : "" }
+      
     </div>
       );
 }
